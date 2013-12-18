b58cf4f898c7a0dbf6ab7a4dd881cdb30dcc252f
math.rondot@gmail.com <math.rondot@gmail.com@5e2d6a4c-1a97-8024-74a1-69ef4713db55>
2013-12-18 15:26:55 +0000

    quizz MAJ Connection class

---
+++ trunk/QUIZZ/src/quizz/Connection.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizz;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Mama
+ */
+public class Connection extends JButton implements ActionListener
+{
+    Connection(String str)
+    {
+        super(str);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
