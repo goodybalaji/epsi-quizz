d03789a1222a11506239835bf92ac51f5c4a28f5
math.rondot@gmail.com <math.rondot@gmail.com@5e2d6a4c-1a97-8024-74a1-69ef4713db55>
2014-03-26 13:03:01 +0000

    MAJ DU 26/03/14

--- trunk/QUIZZ/src/quizz/ConnectionBtn.java
+++ trunk/QUIZZ/src/quizz/ConnectionBtn.java
@@ -33,32 +33,45 @@ public class ConnectionBtn extends JButton implements ActionListener {
   
     @Override
     public void actionPerformed(ActionEvent e) {
-        if ("btnConnection".equals(this.getName()))
+        if ("Connexion".equals(this.getText()))
         {
-            char[] pwd = connectionScreen.txtPwd.getPassword();
-            try {
-                final java.sql.Statement statement = DBConnect.Connect();
-                ResultSet rs = statement.executeQuery("SELECT idPersonne, loginPersonne, mdpPersonne from PERSONNNE, JOUEUR where PERSONNE.idPersonne = JOUEUR.idJoueur " 
-                                                    + "AND PERSONNE.loginPersonne =" + connectionScreen.txtUser.toString()
-                                                    + "AND PERSONNE.mdpPersonne = " + String.valueOf(pwd));
-                if (rs.next() == true)
+           //vérifcation que les champs ne soient pas vide
+            if (!"".equals(connectionScreen.txtUser.getText()))
+           {
+               char[] pwd = connectionScreen.txtPwd.getPassword(); 
+               if (!"".equals(pwd.toString()))
                 {
-                    player = new Player(rs.getCharacterStream("loginPersonne").toString(), rs.getInt(1));
+                    //récupération des informations de l'utilisateur via son login et mdp
+                    try
+                    {
+                        //création de la variable de connexion
+                        final java.sql.Statement statement = DBConnect.Connect();
+                        ResultSet rs = statement.executeQuery("SELECT idPersonne, loginPersonne, mdpPersonne from PERSONNNE, JOUEUR where PERSONNE.idPersonne = JOUEUR.idJoueur "
+                                + "AND PERSONNE.loginPersonne =" + connectionScreen.txtUser.toString()
+                                + "AND PERSONNE.mdpPersonne = " + String.valueOf(pwd));
+                  //si il y a un retour c'est que les identifants sont valide
+                        //création de l'utilisateur et l'on affiche la page suivante.
+                        if (rs.next() == true)
+                        {
+                            player = new Player(rs.getCharacterStream("loginPersonne").toString(), rs.getInt(1));
+                            connectionScreen.setVisible(false);
+                            playerScreenHome.setVisible(true);                            
+                        }
+                        //envoi du message d'erreur et réhinitialisation des champs à vide
+                        else
+                        {                           
+                            JOptionPane.showMessageDialog(connectionScreen, "Connexion imposible, veuillez vérifier vos identifiants");
+                            connectionScreen.txtUser.setText("");
+                            connectionScreen.txtPwd.setText("");                            
+                        }
+                    }
+                    catch (SQLException ex)
+                    {
+                        Logger.getLogger(ConnectionBtn.class.getName()).log(Level.SEVERE, null, ex);
+                    } 
                 }
-                else
-                {
-                    JOptionPane.showMessageDialog(connectionScreen, "Connexion imposible, veuillez vérifier vos identifiants");
-                    connectionScreen.txtUser.setText("");
-                    connectionScreen.txtPwd.setText("");
-                    
-                }
-            } catch (SQLException ex) {
-                Logger.getLogger(ConnectionBtn.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            connectionScreen.setVisible(false);
-
-            playerScreenHome.setVisible(true);
-
+           }
+            
         }
         else if ("btnDeco".equals(this.getName()))
         {
@@ -68,19 +81,25 @@ public class ConnectionBtn extends JButton implements ActionListener {
         }
         else if ("Créer".equals(this.getText()))
         {   
+            //création des variable de verification du mdp
             char[] pwd = connectionScreenAddUser.txtPwd.getPassword();
             char[] pwd2 = connectionScreenAddUser.txtPwd2.getPassword();
+            //vérification du mdp
             if (String.valueOf(pwd).equals(String.valueOf(pwd2)))
             {
                 try
                 {
+                    //création de la variable de connexion
                     final java.sql.Statement statement = DBConnect.Connect();
+                    //vérification de la disponbilité du login du nouvel utilisateur
                     ResultSet rs = statement.executeQuery("SELECT idPersonne, loginPersonne from PERSONNE, JOUEUR where PERSONNE.idPersonne = JOUEUR.idJoueur");
                     while(rs.next() == true){
+                        //si disponible
                         if (rs.getCharacterStream("loginPersonne").toString().equals(connectionScreenAddUser.txtUser.getText()))
                         {
                             creation = true;
                         }
+                        //si pas disponible
                         else
                         {
                             JOptionPane.showMessageDialog(connectionScreenAddUser, "Login utilisé, veuilliez en choisir un autre.");
@@ -89,30 +108,34 @@ public class ConnectionBtn extends JButton implements ActionListener {
                             connectionScreenAddUser.txtPwd2.setText("");
                         }
                     }
+                    //création du nouvel utilisateur
                     if (creation == true)
                     {   
                         creation = false;
+                        
+                        //création de l'utilisateur dans la table PERSONNE
                         String queryCreaPers = "Insert Into PERSONNE (IDPERSONNE, LOGINPERSONNE, MDPPERSONNE) "
                                                    + "VALUES (inc_personne_seq.NEXTVAL, '" + connectionScreenAddUser.txtUser.getText() + "', '" 
                                                    + String.valueOf(pwd) + "')";                        
                         rs = statement.executeQuery(queryCreaPers);
                         
+                        //Recuperation de l'id de PERSONNE
                         queryCreaPers = "SELECT * from PERSONNE WHERE LOGINPERSONNE = '" + connectionScreenAddUser.txtUser.getText() + "'";
-                        System.out.println(queryCreaPers);
-                        
+                        System.out.println(queryCreaPers);                        
                         ResultSet rsSelectId = statement.executeQuery(queryCreaPers);
                         rsSelectId.next();
-                        
-                       
                         int idpersonne = rsSelectId.getInt("IDPERSONNE");
                         
+                        //Création du NbQuizJoueur
                         String queryCreaJou = "insert into nb_quiz_joueur (idnbquiz, NBFACILE, NBMOYEN, NBDIFFICILE) Values (inc_nbquiz_seq.NEXTVAL, 0, 0, 0)";
                         rs = statement.executeQuery(queryCreaJou);  
+                        //recuparation de l'id du nbQuizJoueur
                         queryCreaJou = "SELECT MAX(IDNBQUIZ) from NB_QUIZ_JOUEUR";
                         rsSelectId = statement.executeQuery(queryCreaJou);
                         rsSelectId.next();
                         int idnbquizz = rsSelectId.getInt(1);
                         
+                        //création du JOUEUR avec l'id PERSONNE et l'id NbQuizJoueur
                         queryCreaJou = "Insert Into JOUEUR (IDJOUEUR, IDNBQUIZ) Values (" + idpersonne + ","+ idnbquizz +")";
                         System.out.println(queryCreaJou);
                         rs = statement.executeQuery(queryCreaJou);    
