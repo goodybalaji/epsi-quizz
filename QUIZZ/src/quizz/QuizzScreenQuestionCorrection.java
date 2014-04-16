a732de12b0afae4a558fdf012638363a0958a668
yoan.monet <yoan.monet@gmail.com>
2014-04-16 12:39:58 +0000

    Ecran de correction de question modifié 16/04/14

--- QUIZZ/src/quizz/QuizzScreenQuestionCorrection.java
+++ QUIZZ/src/quizz/QuizzScreenQuestionCorrection.java
@@ -4,39 +4,62 @@
  */
 package quizz;
 
-
 import java.awt.BorderLayout;
+import java.awt.Color;
+import static java.awt.Color.WHITE;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Insets;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import javax.swing.*;
-
+import javax.swing.border.EmptyBorder;
+import quizz.CustomFont;
 /**
  *
- * @author Mama
+ * @author Mama  
  */
-public class QuizzScreenQuestionCorrection extends JFrame
-{
+public class QuizzScreenQuestionCorrection extends JFrame {
+
+    public static CustomFont CustomFont;
+
     public ArrayList<JButton> questionBtnList = new ArrayList<JButton>();
-    public int i;    
-    public JLabel lblQuizzName = new JLabel("[Nom du QUIZZ]");
-    public JLabel lblNbQuestion = new JLabel("Question : X/Y");
+
+    public int i; 
+
+    public static int numQuestion = 1;
+
+
+    public JLabel lblQuizzName = new JLabel("[Nom du Qwizz]", JLabel.CENTER);
+
+   // public JLabel lblQuizzName = new JLabel("Nom du Qwizz");
+
+    public JLabel lblNbQuestion = new JLabel("Question : "+ numQuestion +"/40", JLabel.CENTER);
     public JLabel lblQuestion= new JLabel("Question : [ ... ] ");
     public JLabel lblRep1 = new JLabel("Solution 1 : [ ... ] ");
     public JLabel lblRep2 = new JLabel("Solution 2 : [ ... ] ");
     public JLabel lblRep3 = new JLabel("Solution 3 : [ ... ] ");
+    public JLabel lblTimer = new JLabel("XX:YY");
     public JCheckBox cbxQ1 = new JCheckBox();
     public JCheckBox cbxQ2 = new JCheckBox();
     public JCheckBox cbxQ3 = new JCheckBox();
-    public QuizzBtn btnQuitterPlayer = new QuizzBtn("Quitter");
-    public QuizzBtn btnNextCorrection = new QuizzBtn("  Suivant  ");    
-    public QuizzBtn btnPreviousCorrection = new QuizzBtn("Précédent");
+    public QuizzBtn btnQuitCorrection = new QuizzBtn("Quitter");
+    public SwitchBtn btnNextCorrection = new SwitchBtn("  Suivant  ");    
+    public SwitchBtn btnPreviousCorrection = new SwitchBtn("Précédent");
+    public Font CFont;
     
     public JPanel top = new JPanel();
-    public JPanel topLbl = new JPanel();
-    public JPanel topLblBox = new JPanel();
-    public JPanel topQuestion = new JPanel();
+    public JPanel topCenter = new JPanel();
+    public JPanel topSouth = new JPanel();
+    public JPanel topEast = new JPanel();
+    public JPanel topWest = new JPanel();
+    public JPanel topCenterC = new JPanel();
+    public JPanel topEastC = new JPanel();
+    public JPanel topWestC = new JPanel();
     public JPanel topQuestionList1 = new JPanel();
     public JPanel topQuestionList2 = new JPanel();
     
@@ -45,13 +68,32 @@ public class QuizzScreenQuestionCorrection extends JFrame
     public JPanel panelCenterRep1 = new JPanel();
     public JPanel panelCenterRep2 = new JPanel();
     public JPanel panelCenterRep3 = new JPanel();
+    public JPanel background = new JPanel();
     
     public JPanel bottom = new JPanel();
     public JPanel theBottom = new JPanel();
     public JPanel underBottom = new JPanel();
-
-    QuizzScreenQuestionCorrection() 
+    
+    
+    QuizzScreenQuestionCorrection()
     {
+
+        
+
+        this.setContentPane(new JLabel(new ImageIcon(this.getClass().getResource("\\Resources\\QuizCorrectionBG.png"))));
+
+
+        
+        CustomFont = new CustomFont();
+        CFont = CustomFont.getFont("Hanged Letters.ttf"); 
+        /*
+        addWindowListener( new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				fermer();
+			}
+		});
+        */
+
         for(i=1; i<=41; i++)
         {
             JButton btn = new JButton("" + i);
@@ -62,57 +104,103 @@ public class QuizzScreenQuestionCorrection extends JFrame
             questionBtnList.add(btn);               
         }
         
-        lblQuizzName.setFont(lblQuizzName.getFont().deriveFont(20.0f));
+        
+        lblQuizzName.setForeground(WHITE);
+        lblQuizzName.setFont(CFont.deriveFont(32.0f));
         lblNbQuestion.setFont(lblNbQuestion.getFont().deriveFont(18.0f));
         lblQuestion.setFont(lblQuestion.getFont().deriveFont(16.0f));
+        lblTimer.setFont(lblTimer.getFont().deriveFont(16.0f));
         
-        topLblBox.add(lblQuizzName);
-        topLblBox.add(lblNbQuestion);
-        topLblBox.setLayout(new BoxLayout(topLblBox, BoxLayout.Y_AXIS));
-        topLbl.add(topLblBox);
+        topWestC.setOpaque(false);
+        topWest.add(topWestC);
+        topWest.setOpaque(false);
+        topWestC.setLayout(new BoxLayout(topWestC, BoxLayout.Y_AXIS));
+        topCenterC.add(lblQuizzName);
+        topCenterC.setBorder(new EmptyBorder(-10,0,0,0));
+        topCenterC.add(lblNbQuestion);
+        topCenterC.setLayout(new BoxLayout(topCenterC, BoxLayout.Y_AXIS));
+        topCenterC.setOpaque(false);
+        topCenter.add(topCenterC);
+        topCenter.setOpaque(false);
+        topEastC.setOpaque(false);
+        topEast.add(topEastC);
+        topEast.setOpaque(false);
+        topEastC.setLayout(new BoxLayout(topEastC, BoxLayout.Y_AXIS));
         for(i=0; i<=20; i++)
         {
             topQuestionList1.add(questionBtnList.get(i));
+            topQuestionList1.setOpaque(false);
+            topQuestionList1.setBorder(new EmptyBorder(0,0,-3,0));
         }
         for(i=20; i<=39; i++)
         {
             topQuestionList2.add(questionBtnList.get(i));
+            topQuestionList2.setOpaque(false);
         }
-        topQuestion.add(topQuestionList1);
-        topQuestion.add(topQuestionList2);
-        topQuestion.setLayout(new BoxLayout(topQuestion, BoxLayout.Y_AXIS));
+
+        topSouth.add(topQuestionList1);
+        topSouth.add(topQuestionList2);
+        topSouth.setOpaque(false);
+        topSouth.setLayout(new BoxLayout(topSouth, BoxLayout.Y_AXIS));
         
-        top.add(topLbl);
-        top.add(new JSeparator(SwingConstants.HORIZONTAL));
-        top.add(topQuestion);
-        top.add(new JSeparator(SwingConstants.HORIZONTAL));
-        top.setLayout(new BoxLayout(top, BoxLayout.Y_AXIS));
+        topWest.setPreferredSize(new Dimension(150, topCenter.getSize().height));
+        topEast.setPreferredSize(new Dimension(150, topCenter.getSize().height));
         
+        
+        top.setLayout(new BorderLayout());        
+        top.add(BorderLayout.CENTER, topCenter);
+        top.add(BorderLayout.SOUTH, topSouth);
+        top.add(BorderLayout.WEST, topWest);
+        top.add(BorderLayout.EAST, topEast);
+        top.setOpaque(false);
+        top.setBorder(new EmptyBorder(-2,0,0,0));
      
         panelCenterQuestion.add(lblQuestion);
+        panelCenterQuestion.setOpaque(false);
         center.add(panelCenterQuestion);
         panelCenterRep1.add(lblRep1);
         panelCenterRep1.add(cbxQ1);
+        cbxQ1.setOpaque(false);
+        panelCenterRep1.setOpaque(false);
         center.add(panelCenterRep1);
         panelCenterRep2.add(lblRep2);
         panelCenterRep2.add(cbxQ2);
+        cbxQ2.setOpaque(false);
+        panelCenterRep2.setOpaque(false);
         center.add(panelCenterRep2);
         panelCenterRep3.add(lblRep3);
         panelCenterRep3.add(cbxQ3);
+        cbxQ3.setOpaque(false);
+        panelCenterRep3.setOpaque(false);
         center.add(panelCenterRep3);
-        center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));     
+        center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
+        center.setOpaque(false);
           
-        theBottom.add(btnQuitterPlayer);
-        theBottom.add(new JLabel("                                                                                                                                     "));
+        theBottom.add(btnQuitCorrection);
+        theBottom.add(new JLabel("                                                                                                                                "));
         theBottom.add(btnPreviousCorrection);
         theBottom.add(new JLabel(" "));
+        theBottom.add(new JLabel(" "));
         theBottom.add(btnNextCorrection);
+        theBottom.setOpaque(false);
         underBottom.add(new JLabel(""));
+        underBottom.setOpaque(false);
         bottom.add(theBottom);
         bottom.add(underBottom);
         bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
+        bottom.setOpaque(false);
         
-        this.setTitle("QUIZZ : Corection du QUIZZ");
+        btnQuitCorrection.addActionListener(btnQuitCorrection);
+        btnNextCorrection.addActionListener(btnNextCorrection);
+        btnPreviousCorrection.addActionListener(btnPreviousCorrection);
+        
+        background.add(top);
+        background.add(center);
+        background.add(bottom);
+        background.setLayout(new BoxLayout(background, BoxLayout.Y_AXIS));
+                
+        this.setTitle("QWIZZ : Correction");
+        setLayout(new BorderLayout());
         this.setSize(700,400);  
         this.setResizable(false);
         this.getContentPane().add(BorderLayout.SOUTH, bottom);
@@ -121,7 +209,22 @@ public class QuizzScreenQuestionCorrection extends JFrame
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-    }  
+    }
     
+    public class SwitchBtn extends JButton implements ActionListener
+    {
+        SwitchBtn(String str)
+        {
+            super(str);
+        }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if("  Suivant  ".equals(this.getText()))
+        {
+            numQuestion++;
+            this.repaint();
+        }
+        }
+    }
 }
