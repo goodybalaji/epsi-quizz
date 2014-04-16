ec58e6db9550e3a94cf27a29f021dd47f3fb7833
yoan.monet <yoan.monet@gmail.com>
2014-04-16 12:04:02 +0000

    Ecran de création de question modifié 16/04/14

--- QUIZZ/src/quizz/QuizzScreenQuestionCreation.java
+++ QUIZZ/src/quizz/QuizzScreenQuestionCreation.java
@@ -9,6 +9,7 @@ import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,7 +24,7 @@ import javax.swing.border.EmptyBorder;
 public class QuizzScreenQuestionCreation extends JFrame
 {
     public int numQuestion = 1;
-    public JLabel lbl1 = new JLabel("Création de QUIZZ - Ajout de question");
+    public JLabel lbl1 = new JLabel(" ");
     public JLabel lbl2 = new JLabel("Question N° "+ numQuestion + " : ");
     public JLabel lblImage = new JLabel("Image (facultatif) : ");
     public JLabel lblNB = new JLabel(" Cochez la réponse correcte ! ");
@@ -61,6 +62,7 @@ public class QuizzScreenQuestionCreation extends JFrame
     public JPanel panelCenterRep3 = new JPanel();
     public JPanel panelCenterRep4 = new JPanel();
     public JPanel panelCenterRep5 = new JPanel();
+    public JPanel background = new JPanel();
     
     public JPanel bottom = new JPanel();
     public JPanel theBottom = new JPanel();
@@ -68,6 +70,7 @@ public class QuizzScreenQuestionCreation extends JFrame
     
     QuizzScreenQuestionCreation()
     {
+        this.setContentPane(new JLabel(new ImageIcon(this.getClass().getResource("\\Resources\\QuestionCreationBG.png"))));
         lbl1.setFont(lbl1.getFont().deriveFont(24.0f));
         txtQuestion.setPreferredSize(new Dimension( 550, 25));
         imageQuestion.setPreferredSize(new Dimension( 350, 25));
@@ -85,8 +88,11 @@ public class QuizzScreenQuestionCreation extends JFrame
         });
         
         topLbl.add(lbl1); 
+        topLbl.setOpaque(false);
         top.add(topLbl);
         top.setLayout(new BoxLayout(top, BoxLayout.Y_AXIS));
+        top.setOpaque(false);
+        top.setBorder(new EmptyBorder(0,0,35,0));
         
         panelCenterQuestion.add(lbl2);
         panelCenterQuestion.add(txtQuestion);
@@ -95,24 +101,30 @@ public class QuizzScreenQuestionCreation extends JFrame
         panelCenterQuestion.add(btnAddURL);
         panelCenterQuestion.add(lblNB);
         panelCenterQuestion.setBorder(new EmptyBorder(0,0,60,0));
+        panelCenterQuestion.setOpaque(false);
         center.add(panelCenterQuestion);
         panelCenterRep1.add(lbl3);
         panelCenterRep1.add(txtRep1);
         panelCenterRep1.add(cbxQ1);
+        panelCenterRep1.setOpaque(false);
         center.add(panelCenterRep1);        
         panelCenterRep2.add(lbl4);
         panelCenterRep2.add(txtRep2);
         panelCenterRep2.add(cbxQ2);
+        panelCenterRep2.setOpaque(false);
         center.add(panelCenterRep2); 
         panelCenterRep3.add(lbl5);
         panelCenterRep3.add(txtRep3);
         panelCenterRep3.add(cbxQ3);
+        panelCenterRep3.setOpaque(false);
         center.add(panelCenterRep3); 
         panelCenterRep4.add(lbl6);
         panelCenterRep4.add(txtRep4);
         panelCenterRep4.add(cbxQ4);
+        panelCenterRep4.setOpaque(false);
         center.add(panelCenterRep4); 
         panelCenterRep5.setBorder(new EmptyBorder(0,0,100,0));
+        panelCenterRep5.setOpaque(false);
         /*panelCenterRep5.add(txtRep5);
         panelCenterRep5.add(cbxQ5);
         center.add(panelCenterRep5);*/ 
@@ -122,18 +134,28 @@ public class QuizzScreenQuestionCreation extends JFrame
         
         center.add(panelCenterRep5);
         center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
+        center.setOpaque(false);
         
         theBottom.add(btnQuitterAdmin);
         theBottom.add(new JLabel("                                                                                                                                     "));
         theBottom.add(btnPreviousQuestionCreation);
         theBottom.add(new JLabel(" "));
         theBottom.add(btnNextQuestionCreation);
+        theBottom.setOpaque(false);
         underBottom.add(new JLabel(""));
+        underBottom.setOpaque(false);
         bottom.add(theBottom);
         bottom.add(underBottom);
         bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
+        bottom.setOpaque(false);
+        
+        background.add(top);
+        background.add(center);
+        background.add(bottom);
+        background.setLayout(new BoxLayout(background, BoxLayout.Y_AXIS));
         
         this.setTitle("QUIZZ : Création du QUIZZ");
+        setLayout(new BorderLayout());
         this.setSize(700,400);
         this.setResizable(false);
         this.getContentPane().add(BorderLayout.SOUTH, bottom);
