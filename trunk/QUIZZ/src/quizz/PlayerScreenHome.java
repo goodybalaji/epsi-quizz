5cf7a94cc4a7e3b863e6061a9dfbdcd4cfaefc21
math.rondot@gmail.com <math.rondot@gmail.com@5e2d6a4c-1a97-8024-74a1-69ef4713db55>
2014-05-14 12:49:41 +0000

    MAJ DU 14/05/14

--- trunk/QUIZZ/src/quizz/PlayerScreenHome.java
+++ trunk/QUIZZ/src/quizz/PlayerScreenHome.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package quizz;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
@@ -18,16 +19,20 @@ import java.sql.SQLException;
 import java.util.regex.PatternSyntaxException;
 import javax.swing.*;
 import static javax.swing.ListSelectionModel.SINGLE_SELECTION;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
+import javax.swing.text.TextAction;
+
 /**
  *
  * @author Arc
  */
-public class PlayerScreenHome extends JFrame
-{
+public class PlayerScreenHome extends JFrame {
+
     public PlayerBtn btnDeco = new PlayerBtn("Déconnexion");
     public JLabel lbl2 = new JLabel("Nom : ");
     public JTextField txtNameQuizz = new JTextField();
@@ -38,16 +43,15 @@ public class PlayerScreenHome extends JFrame
     public PlayerBtn btnStatPlayer = new PlayerBtn("Statistiques");
     public PlayerBtn btnRankPlayer = new PlayerBtn("Classement");
     public PlayerBtn btnPlayPlayer = new PlayerBtn("Jouer");
-    
-    
+
     public JPanel top = new JPanel();
     public JPanel center = new JPanel();
-    
+
     public JPanel centerTop = new JPanel();
     public JPanel centerBottom = new JPanel();
     public JPanel centerUnderBottom = new JPanel();
     public JPanel bottom = new JPanel();
-    
+
     public JPanel topCenter = new JPanel();
     public JPanel topCenterC = new JPanel();
     public JPanel topEast = new JPanel();
@@ -58,32 +62,40 @@ public class PlayerScreenHome extends JFrame
     public JPanel centerTopName = new JPanel();
     public JPanel centerTopTheme = new JPanel();
     public JPanel centerTopCheck = new JPanel();
-   
+
     public JPanel JPlbl8 = new JPanel();
     public JTable tableau;
     public QwizTableModel data;
     TableRowSorter<TableModel> sorter;
     public int selectedRow;
     public String idQuiz;
-   
+
     public int nb;
     public JPanel bottomBtnStat = new JPanel();
     public JPanel bottomBtnClass = new JPanel();
     public JPanel bottomBtnPlay = new JPanel();
     public JPanel background = new JPanel();
-    
-    public void mouseClicked(MouseEvent e) {    }
-    public void mousePressed(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
     @SuppressWarnings("empty-statement")
-    PlayerScreenHome() throws SQLException
-    {   
+    PlayerScreenHome() throws SQLException {
         this.setContentPane(new JLabel(new ImageIcon(this.getClass().getResource("\\Resources\\HomeBG.png"))));
         final java.sql.Statement statement = DBConnect.Connect();
-        
+
         topCenterC.add(new JLabel(" "));
         topCenterC.add(new JLabel(" "));
         topCenterC.setOpaque(false);
@@ -95,42 +107,39 @@ public class PlayerScreenHome extends JFrame
         topEast.add(topEastC);
         topEast.setOpaque(false);
         topEastC.setLayout(new BoxLayout(topEastC, BoxLayout.Y_AXIS));
-       
+
         topSouth.setLayout(new BoxLayout(topSouth, BoxLayout.Y_AXIS));
         topSouth.setOpaque(false);
-        
+
         topWest.setPreferredSize(new Dimension(150, topCenter.getSize().height));
         topEast.setPreferredSize(new Dimension(150, topCenter.getSize().height));
         topWest.setOpaque(false);
-        
-        
-        top.setLayout(new BorderLayout());        
+
+        top.setLayout(new BorderLayout());
         top.add(BorderLayout.CENTER, topCenter);
         top.add(BorderLayout.SOUTH, topSouth);
         top.add(BorderLayout.WEST, topWest);
         top.add(BorderLayout.EAST, topEast);
         top.setOpaque(false);
-        
+
         btnDeco.addActionListener(btnDeco);
-        
+
         //CENTER
         cbxTheme.addItem("");
         ResultSet rs = statement.executeQuery("Select * from THEME");
-            while(rs.next() == true)
-            {
-                cbxTheme.addItem(rs.getString("lblTheme"));
-            }
-            
+        while (rs.next() == true) {
+            cbxTheme.addItem(rs.getString("lblTheme"));
+        }
+
         cbxDiffilute.addItem("");
         rs = statement.executeQuery("Select * from Difficulte");
-            while(rs.next() == true)
-            {
-                cbxDiffilute.addItem(rs.getString("lbldifficulte"));
-            }
-            
-        txtNameQuizz.setPreferredSize(new Dimension( 190, 25));
-        cbxTheme.setPreferredSize(new Dimension( 140, 25));
-        cbxDiffilute.setPreferredSize(new Dimension( 140, 25));
+        while (rs.next() == true) {
+            cbxDiffilute.addItem(rs.getString("lbldifficulte"));
+        }
+
+        txtNameQuizz.setPreferredSize(new Dimension(190, 25));
+        cbxTheme.setPreferredSize(new Dimension(140, 25));
+        cbxDiffilute.setPreferredSize(new Dimension(140, 25));
         centerTopName.add(lbl2);
         centerTopName.add(txtNameQuizz);
         centerTopName.setOpaque(false);
@@ -144,66 +153,81 @@ public class PlayerScreenHome extends JFrame
         centerTop.add(centerTopTheme);
         centerTop.add(centerTopCheck);
         centerTop.setOpaque(false);
-        
-        cbxTheme.addItemListener(new ItemListener()
-        {
-            
+
+        cbxTheme.addItemListener(new ItemListener() {
+
             @Override
-            public void itemStateChanged(ItemEvent e)
-            {
-                if (e.getStateChange() == ItemEvent.SELECTED)
-                {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
                     //tableau = null;
                     Object item = e.getItem();
-                   if (item.toString().equals(""))
-                   {
+                    if (item.toString().equals("")) {
                         sorter.setRowFilter(null);
-                   }
-                   else
-                   {
-                      try 
-                      {
-                        sorter.setRowFilter(RowFilter.regexFilter(item.toString(),1));
-                      }
-                      catch (PatternSyntaxException pse)
-                      {
-                        System.err.println("Bad regex pattern");
-                      } 
-                   }  
+                    } else {
+                        try {
+                            sorter.setRowFilter(RowFilter.regexFilter(item.toString(), 1));
+                        } catch (PatternSyntaxException pse) {
+                            System.err.println("Bad regex pattern");
+                        }
+                    }
                 }
             }
         });
-        
-        cbxDiffilute.addItemListener(new ItemListener()
-        {
-            
+
+        cbxDiffilute.addItemListener(new ItemListener() {
+
             @Override
-            public void itemStateChanged(ItemEvent e)
-            {
-                if (e.getStateChange() == ItemEvent.SELECTED)
-                {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
                     //tableau = null;
                     Object item = e.getItem();
-                   if (item.toString().equals(""))
-                   {
+                    if (item.toString().equals("")) {
                         sorter.setRowFilter(null);
-                   }
-                   else
-                   {
-                      try 
-                      {
-                        sorter.setRowFilter(RowFilter.regexFilter(item.toString(),2));
-                      }
-                      catch (PatternSyntaxException pse)
-                      {
-                        System.err.println("Bad regex pattern");
-                      } 
-                   }   
+                    } else {
+                        try {
+                            sorter.setRowFilter(RowFilter.regexFilter(item.toString(), 2));
+                        } catch (PatternSyntaxException pse) {
+                            System.err.println("Bad regex pattern");
+                        }
+                    }
                 }
             }
         });
-        
-        
+
+        txtNameQuizz.getDocument().addDocumentListener(new DocumentListener() {
+            RowFilter<TableModel, Object> rf = null;
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                try {
+                    rf = RowFilter.regexFilter(txtNameQuizz.getText(), 0);
+                } catch (java.util.regex.PatternSyntaxException error) {
+                    return;
+                }
+                sorter.setRowFilter(rf);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+               try {
+                    rf = RowFilter.regexFilter(txtNameQuizz.getText(), 0);
+                } catch (java.util.regex.PatternSyntaxException error) {
+                    return;
+                }
+                sorter.setRowFilter(rf);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                try {
+                    rf = RowFilter.regexFilter(txtNameQuizz.getText(), 0);
+                } catch (java.util.regex.PatternSyntaxException error) {
+                    return;
+                }
+                sorter.setRowFilter(rf);
+            }
+        });
+
         centerBottom.setOpaque(false);
         rs = statement.executeQuery("Select count(nomquiz) "
                 + "from QUIZ Q");
@@ -215,38 +239,35 @@ public class PlayerScreenHome extends JFrame
         rs = statement.executeQuery("SELECT Q.idQuiz, Q.nomQuiz, T.lblTheme, D.lblDifficulte, Q.dateCreaQuiz "
                 + "FROM QUIZ Q, THEME T, DIFFICULTE D WHERE Q.idDifficulte = D.idDifficulte "
                 + "AND Q.idTheme = T.idTheme ORDER BY Q.nomQuiz");
-        int i=0;        
-        while(rs.next() == true){
+        int i = 0;
+        while (rs.next() == true) {
             data.setValueAt(rs.getString("nomQuiz"), i, 0);
             data.setValueAt(rs.getString("lblTheme"), i, 1);
             data.setValueAt(rs.getString("lblDifficulte"), i, 2);
             data.setValueAt(setDate(rs.getDate("dateCreaQuiz").toString()), i, 3);
-            data.setValueAt(""+rs.getInt("idQuiz"), i, 4);
-            i++;  
-        } 
-        
-        ListSelectionModel listSelectionModel = tableau.getSelectionModel();        
-        listSelectionModel.addListSelectionListener(new ListSelectionListener()
-        {
+            data.setValueAt("" + rs.getInt("idQuiz"), i, 4);
+            i++;
+        }
+
+        ListSelectionModel listSelectionModel = tableau.getSelectionModel();
+        listSelectionModel.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-                if (e.getValueIsAdjusting())
+                if (e.getValueIsAdjusting()) {
                     return;
-                ListSelectionModel lsm = (ListSelectionModel)e.getSource();
-                if (lsm.isSelectionEmpty())
-                {
-                    System.out.println("No rows selected");
                 }
-            else
-                {
+                ListSelectionModel lsm = (ListSelectionModel) e.getSource();
+                if (lsm.isSelectionEmpty()) {
+                    System.out.println("No rows selected");
+                } else {
                     selectedRow = lsm.getMinSelectionIndex();
                     idQuiz = data.getValueAt(selectedRow, 4).toString();
                 }
             }
         });
-        
-        JPlbl8.setPreferredSize(new Dimension( 670, 230));
-        tableau.setPreferredSize(new Dimension( 665, tableau.getRowCount()*16));
+
+        JPlbl8.setPreferredSize(new Dimension(670, 230));
+        tableau.setPreferredSize(new Dimension(665, tableau.getRowCount() * 16));
         tableau.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         tableau.getColumnModel().getColumn(0).setPreferredWidth(250);
         tableau.getColumnModel().getColumn(1).setPreferredWidth(130);
@@ -260,7 +281,7 @@ public class PlayerScreenHome extends JFrame
         tableau.setCellSelectionEnabled(false);
         tableau.setCellEditor(null);
         JScrollPane scrollPane = new JScrollPane(tableau);
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);               
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setPreferredSize(new Dimension(665, 215));
         JPlbl8.add(scrollPane);
@@ -271,60 +292,54 @@ public class PlayerScreenHome extends JFrame
         center.add(centerBottom);
         center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
         center.setOpaque(false);
-        
-        
+
         //BOTTOM
-        btnStatPlayer.setPreferredSize(new Dimension( 120, 50));
+        btnStatPlayer.setPreferredSize(new Dimension(120, 50));
         btnStatPlayer.addActionListener(btnStatPlayer);
-        btnRankPlayer.setPreferredSize(new Dimension( 120, 50));
+        btnRankPlayer.setPreferredSize(new Dimension(120, 50));
         btnRankPlayer.addActionListener(btnRankPlayer);
-        btnPlayPlayer.setPreferredSize(new Dimension( 120, 50));
+        btnPlayPlayer.setPreferredSize(new Dimension(120, 50));
         btnPlayPlayer.addActionListener(btnPlayPlayer);
         btnDeco.addActionListener(btnDeco);
-        
+
         bottomBtnStat.add(btnStatPlayer);
         bottomBtnStat.setOpaque(false);
         bottomBtnClass.add(btnRankPlayer);
         bottomBtnClass.setOpaque(false);
         bottomBtnPlay.add(btnPlayPlayer);
         bottomBtnPlay.setOpaque(false);
-        
+
         bottom.add(bottomBtnStat);
         bottom.add(bottomBtnClass);
         bottom.add(bottomBtnPlay);
         bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
         bottom.setOpaque(false);
-        
+
         background.add(top);
         background.add(center);
         background.add(bottom);
         background.setLayout(new BoxLayout(background, BoxLayout.Y_AXIS));
-        
-        
+
         this.setTitle("QWIZZ : Accueil");
         setLayout(new BorderLayout());
-        this.setSize(700,400);
+        this.setSize(700, 400);
         this.setResizable(false);
         this.getContentPane().add(BorderLayout.SOUTH, bottom);
         this.getContentPane().add(BorderLayout.NORTH, top);
         this.getContentPane().add(BorderLayout.CENTER, center);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
-   
-     public static String setDate(String str)
-    {
-       char[] Char = new char[10];
-       str.getChars(8, 10, Char, 0);
-       Char[2] += '/';
-       str.getChars(5, 7, Char, 3);
-       Char[5] += '/';
-       str.getChars(0, 4, Char, 6);           
-        return String.valueOf(Char);        
-    }    
-    
-    
-    
+
+    public static String setDate(String str) {
+        char[] Char = new char[10];
+        str.getChars(8, 10, Char, 0);
+        Char[2] += '/';
+        str.getChars(5, 7, Char, 3);
+        Char[5] += '/';
+        str.getChars(0, 4, Char, 6);
+        return String.valueOf(Char);
+    }
+
 }
