7c7f66f7bc259d0c09f56d9cd35d6de3328c3790
math.rondot@gmail.com <math.rondot@gmail.com@5e2d6a4c-1a97-8024-74a1-69ef4713db55>
2014-05-14 13:31:47 +0000

    MAJ DU 14/05/14

--- trunk/QUIZZ/src/quizz/AdminScreenHome.java
+++ trunk/QUIZZ/src/quizz/AdminScreenHome.java
@@ -3,39 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package quizz;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
-import javax.swing.border.Border;
+import static javax.swing.ListSelectionModel.SINGLE_SELECTION;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import static quizz.PlayerScreenHome.setDate;
+import static quizz.QUIZZ.admin;
 
 /**
  *
  * @author Arc
  */
-public class AdminScreenHome extends JFrame
-{
+public class AdminScreenHome extends JFrame {
+
     public JLabel lbl1 = new JLabel(" ");
     public AdminBtn btnDeco = new AdminBtn("Déconnexion");
-    public JLabel lbl4 = new JLabel("Nom du Quizz");
-    public JLabel lbl5 = new JLabel("Theme");
-    public JLabel lbl6 = new JLabel("Date Creation");
-    public JLabel lbl7 = new JLabel("Date MaJ");
-    public JLabel lbl8 = new JLabel("[listeQuizz]");
-    public JLabel lbl9 = new JLabel("Difficulté");
-    String[] head = {"Nom", "Theme", "Difficulté", "Date Création", "Date Maj"};
-    Object[][] data = {};
-    public JTable quizzTable = new JTable(data, head);
+    public JTable tableau;
+    public QwizTableModel data;
+    public int nb;
+    public int selectedRow;
+    public String idQuiz;
     public AdminBtn btnClassAdmin = new AdminBtn("Classement");
     public AdminBtn btnModifAdmin = new AdminBtn("Modifier");
     public AdminBtn btnDelAdmin = new AdminBtn("Supprimer");
     public AdminBtn btnNewAdmin = new AdminBtn("Nouveau Quizz");
     public AdminBtn btnAddAdmin = new AdminBtn("Ajouter Admin");
-    
+
     public JPanel top = new JPanel();
     public JPanel topEast = new JPanel();
     public JPanel topEastC = new JPanel();
@@ -49,27 +52,19 @@ public class AdminScreenHome extends JFrame
     public JPanel centerBottom = new JPanel();
     public JPanel bottom = new JPanel();
     public JPanel background = new JPanel();
-    
-    public JPanel JPlbl1 = new JPanel();
-    public JPanel JPlbl4 = new JPanel();
-    public JPanel JPlbl5 = new JPanel();
-    public JPanel JPlbl6 = new JPanel();
-    public JPanel JPlbl7 = new JPanel();
+
+   
     public JPanel JPlbl8 = new JPanel();
-    public JPanel JPlbl9 = new JPanel();
     public JPanel bottomClassBtn = new JPanel();
     public JPanel bottomModifBtn = new JPanel();
     public JPanel bottomDelBtn = new JPanel();
     public JPanel bottomNewBtn = new JPanel();
-    
 
-    
-    AdminScreenHome()
-    {
+    AdminScreenHome() {
         this.setContentPane(new JLabel(new ImageIcon(this.getClass().getResource("\\Resources\\AdminHome.png"))));
         //TOP
         lbl1.setFont(lbl1.getFont().deriveFont(18.0f));
-        
+
         topCenterC.add(lbl1);
         topCenterC.setLayout(new BoxLayout(topCenterC, BoxLayout.Y_AXIS));
         topCenterC.setOpaque(false);
@@ -85,55 +80,95 @@ public class AdminScreenHome extends JFrame
         topWest.add(topWestC);
         topWest.setOpaque(false);
         topWestC.setLayout(new BoxLayout(topWestC, BoxLayout.Y_AXIS));
-       
+
         topSouth.setLayout(new BoxLayout(topSouth, BoxLayout.Y_AXIS));
         topSouth.setOpaque(false);
-        
+
         topWest.setPreferredSize(new Dimension(150, topCenter.getSize().height));
         topWest.setOpaque(false);
         topEast.setPreferredSize(new Dimension(150, topCenter.getSize().height));
-        
-        top.setLayout(new BorderLayout());        
+
+        top.setLayout(new BorderLayout());
         top.add(BorderLayout.CENTER, topCenter);
         top.add(BorderLayout.SOUTH, topSouth);
         top.add(BorderLayout.WEST, topWest);
         top.add(BorderLayout.EAST, topEast);
         top.setOpaque(false);
-        
+
         //CENTER       
-        JPlbl4.add(lbl4);
-        JPlbl5.add(lbl5);
-        JPlbl6.add(lbl6);
-        JPlbl7.add(lbl7);
-        JPlbl9.add(lbl9);
-        JPlbl4.setPreferredSize(new Dimension( 250, 25));
-        JPlbl5.setPreferredSize(new Dimension( 100, 25));
-        JPlbl6.setPreferredSize(new Dimension( 100, 25));
-        JPlbl7.setPreferredSize(new Dimension( 100, 25));
-        JPlbl8.setPreferredSize(new Dimension( 100, 25));
-        JPlbl9.setPreferredSize(new Dimension( 100, 25));
-        JPlbl4.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        JPlbl5.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        JPlbl6.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        JPlbl7.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        JPlbl9.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        centerTop.add(JPlbl4);
-        centerTop.add(JPlbl5);
-        centerTop.add(JPlbl9);
-        centerTop.add(JPlbl6);
-        centerTop.add(JPlbl7);
-        centerTop.setOpaque(false);
         
-        JPlbl8.setPreferredSize(new Dimension( 670, 230));
-        JPlbl8.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        centerTop.setOpaque(false);
+
+        final java.sql.Statement statement;
+        try {
+            statement = DBConnect.Connect();
+            ResultSet rs = statement.executeQuery("Select count(nomquiz) "
+                    + "from QUIZ Q "
+                    + "Where idAdmin = "+ admin.getId());
+            rs.next();
+            nb = rs.getInt(1);
+            tableau = new JTable(data = new QwizTableModel(nb));
+
+            rs = statement.executeQuery("SELECT Q.idQuiz, Q.nomQuiz, T.lblTheme, D.lblDifficulte, Q.dateCreaQuiz "
+                    + "FROM QUIZ Q, THEME T, DIFFICULTE D WHERE Q.idDifficulte = D.idDifficulte "
+                    + "AND Q.idTheme = T.idTheme And idAdmin = " + admin.getId()
+                    + " ORDER BY Q.nomQuiz");
+            int i = 0;
+            while (rs.next() == true) {
+                data.setValueAt(rs.getString("nomQuiz"), i, 0);
+                data.setValueAt(rs.getString("lblTheme"), i, 1);
+                data.setValueAt(rs.getString("lblDifficulte"), i, 2);
+                data.setValueAt(setDate(rs.getDate("dateCreaQuiz").toString()), i, 3);
+                data.setValueAt("" + rs.getInt("idQuiz"), i, 4);
+                i++;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AdminScreenHome.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        ListSelectionModel listSelectionModel = tableau.getSelectionModel();
+        listSelectionModel.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (e.getValueIsAdjusting()) {
+                    return;
+                }
+                ListSelectionModel lsm = (ListSelectionModel) e.getSource();
+                if (lsm.isSelectionEmpty()) {
+                    System.out.println("No rows selected");
+                } else {
+                    selectedRow = lsm.getMinSelectionIndex();
+                    idQuiz = data.getValueAt(selectedRow, 4).toString();
+                }
+            }
+        });
+
+        JScrollPane scrollPane = new JScrollPane(tableau);
+        JPlbl8.setPreferredSize(new Dimension(670, 230));
+        tableau.setPreferredSize(new Dimension(665, tableau.getRowCount() * 16));
+        tableau.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        tableau.getColumnModel().getColumn(0).setPreferredWidth(250);
+        tableau.getColumnModel().getColumn(1).setPreferredWidth(130);
+        tableau.getColumnModel().getColumn(2).setPreferredWidth(130);
+        tableau.getColumnModel().getColumn(3).setPreferredWidth(130);
+        tableau.getColumnModel().getColumn(4).setPreferredWidth(0);
+        tableau.getSelectionModel().setSelectionMode(SINGLE_SELECTION);
+        tableau.setRowSelectionAllowed(true);
+        tableau.setColumnSelectionAllowed(false);
+        tableau.setCellSelectionEnabled(false);
+        tableau.setCellEditor(null);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setPreferredSize(new Dimension(665, 215));
+        JPlbl8.add(scrollPane);
+        JPlbl8.setOpaque(false);
         centerBottom.add(JPlbl8);
         centerBottom.setOpaque(false);
-
         center.add(centerTop);
         center.add(centerBottom);
         center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
         center.setOpaque(false);
-        
+
         //BOTTOM
         btnNewAdmin.addActionListener(btnNewAdmin);
         btnClassAdmin.addActionListener(btnClassAdmin);
@@ -141,11 +176,11 @@ public class AdminScreenHome extends JFrame
         btnDelAdmin.addActionListener(btnDelAdmin);
         btnAddAdmin.addActionListener(btnAddAdmin);
         btnDeco.addActionListener(btnDeco);
-        btnClassAdmin.setPreferredSize(new Dimension( 120, 50));
-        btnModifAdmin.setPreferredSize(new Dimension( 120, 50));
-        btnDelAdmin.setPreferredSize(new Dimension( 120, 50));
-        btnNewAdmin.setPreferredSize(new Dimension( 120, 50));
-        
+        btnClassAdmin.setPreferredSize(new Dimension(120, 50));
+        btnModifAdmin.setPreferredSize(new Dimension(120, 50));
+        btnDelAdmin.setPreferredSize(new Dimension(120, 50));
+        btnNewAdmin.setPreferredSize(new Dimension(120, 50));
+
         bottomClassBtn.add(btnClassAdmin);
         bottomClassBtn.setOpaque(false);
         bottomModifBtn.add(btnModifAdmin);
@@ -154,28 +189,28 @@ public class AdminScreenHome extends JFrame
         bottomDelBtn.setOpaque(false);
         bottomNewBtn.add(btnNewAdmin);
         bottomNewBtn.setOpaque(false);
-        
+
         bottom.add(bottomClassBtn);
         bottom.add(bottomModifBtn);
         bottom.add(bottomDelBtn);
         bottom.add(bottomNewBtn);
         bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
         bottom.setOpaque(false);
-        
+
         background.add(top);
         background.add(center);
         background.add(bottom);
         background.setLayout(new BoxLayout(background, BoxLayout.Y_AXIS));
-        
+
         this.setTitle("QWIZZ : ACCUEIL Admin");
         setLayout(new BorderLayout());
-        this.setSize(700,400);
+        this.setSize(700, 400);
         this.setResizable(false);
         this.getContentPane().add(BorderLayout.SOUTH, bottom);
         this.getContentPane().add(BorderLayout.NORTH, top);
         this.getContentPane().add(BorderLayout.CENTER, center);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
--- trunk/QUIZZ/src/quizz/QwizTableModel.java
+++ trunk/QUIZZ/src/quizz/QwizTableModel.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package quizz;
 
 import javax.swing.table.AbstractTableModel;
@@ -12,48 +11,40 @@ import javax.swing.table.AbstractTableModel;
  *
  * @author Mama
  */
-public class QwizTableModel extends AbstractTableModel
-{
-    private final String[] columnNames;
-    private final  Object[][] data;
+public class QwizTableModel extends AbstractTableModel {
 
-   
+    private final String[] columnNames;
+    private final Object[][] data;
+
     @Override
-    public int getRowCount()
-    {
+    public int getRowCount() {
         return data.length;
     }
 
     @Override
-    public int getColumnCount()
-    {        
+    public int getColumnCount() {
         return columnNames.length;
     }
 
     @Override
-    public Object getValueAt(int rowIndex, int columnIndex)
-    {
+    public Object getValueAt(int rowIndex, int columnIndex) {
         return this.data[rowIndex][columnIndex];
     }
-    
+
     @Override
-     public void setValueAt(Object value, int row, int col)
-     {
-         this.data[row][col] = value;
-         fireTableCellUpdated(row, col);
-         //fireTableStructureChanged();
-     }
-     
-     
+    public void setValueAt(Object value, int row, int col) {
+        this.data[row][col] = value;
+        fireTableCellUpdated(row, col);
+        //fireTableStructureChanged();
+    }
+
     @Override
-     public String getColumnName(int col) {
+    public String getColumnName(int col) {
         return columnNames[col].toString();
     }
-    
-     
-     QwizTableModel(int nb)
-     { 
-        columnNames = new String[]{"Nom", "Thème", "Difficulté", "Date Création", "id qwizz"};
+
+    QwizTableModel(int nb) {
+        columnNames = new String[]{"Nom du Qwiz", "Thème", "Difficulté", "Date Création", "id qwizz"};
         data = new Object[nb][5];
-     }
+    }
 }
